c4e1823b8b63f87976e17a11a3af58c33ba8cbc9
Trushenkov Dmitry <diman28051999@gmail.com>
2019-02-13 21:48:28 +0300

    added method for open modal window

---
+++ src/sample/Service.java
@@ -0,0 +1,77 @@
+package sample;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.Node;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+
+/**
+ * Date: 12.02.2019 (вторник)
+ * Project name: TestApplication
+ * Package name: sample
+ *
+ * @author Трушенков Дмитрий 15ИТ18
+ */
+public class Service {
+
+    /**
+     * Метод для перехода на другое окно приложения
+     *
+     * @param path  путь к fxml файлу
+     * @param title заголовок окна
+     */
+    public void changeScreen(String path, String title) {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource(path));
+        try {
+            loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Parent root = loader.getRoot();
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        if (path.equals("/sample/view/listMaterialsScreen.fxml")) {
+            scene.getStylesheets().add("/sample/css/style.css");
+        }
+        stage.setTitle("Test application - " + title + "!");
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    /**
+     * Метод для открытия модального окна
+     *
+     * @param path путь к файлу разметки (fxml-file)
+     * @param title заголовок модального окна
+     * @param actionEvent событие
+     */
+    public void changeScreenModalWindow(String path, String title, ActionEvent actionEvent) {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource(path));
+        try {
+            loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Parent root = loader.getRoot();
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.setTitle("Test application - " + title + "!");
+        if (actionEvent != null) {
+            stage.initModality(Modality.WINDOW_MODAL);
+            stage.initOwner(((Node) actionEvent.getSource()).getScene().getWindow());
+        }
+        stage.setResizable(false);
+        stage.show();
+    }
+}
