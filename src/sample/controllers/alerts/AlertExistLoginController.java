709fc376ed8078270130749f05f9e79396cd5ac6
Trushenkov Dmitry <diman28051999@gmail.com>
2019-02-15 16:03:47 +0300

    created controller for modal window

---
+++ src/sample/controllers/alerts/AlertExistLoginController.java
@@ -0,0 +1,46 @@
+package sample.controllers.alerts;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+
+/**
+ * Date: 15.02.2019 (пятница)
+ * Project name: TestApplication
+ * Package name: sample.controllers
+ *
+ * @author Трушенков Дмитрий 15ИТ18
+ */
+public class AlertExistLoginController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Label detailsLabel;
+
+    @FXML
+    private HBox actionParent;
+
+    @FXML
+    private Button okButton;
+
+    @FXML
+    private HBox okParent;
+
+    @FXML
+    private Label messageLabel;
+
+    @FXML
+    void initialize() {
+        okButton.setOnAction(event -> {
+            okButton.getScene().getWindow().hide();
+        });
+    }
+}
